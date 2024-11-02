b5987a871eea38e7814bd0eed5563373281b78ca
makandulu <udinr5181@gmail.com>
2024-11-02 11:21:38 +0700

    no sama

--- proses_daftar.php
+++ proses_daftar.php
@@ -5,22 +5,21 @@ $query = new mysqli('localhost', 'root', '', 'projek');
 
 $username = $_POST['username'];
 $password = $_POST['password'];
-$data = $query->query("INSERT INTO user (username, password) VALUES ('$username', '$password');")
-          or die($query->error);
-          <?php
+
 // Buat query untuk mengecek apakah username dan password sudah ada
 $cek = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
-$result = mysqli_query($connection, $cek);
-
+$result = mysqli_query($query, $cek);
 // Cek apakah ada baris yang ditemukan
 if (mysqli_num_rows($result) > 0) {
     echo "Username dan password sudah ada di database.";
+    
 } else {
     echo "Username dan password belum ada di database.";
+    $data = $query->query("INSERT INTO user (username, password) VALUES ('$username', '$password');")
+          or die($query->error);
 }
-
 // Tutup koneksi
-mysqli_close($connection);
+mysqli_close($query);
 ?>
 
-?>
+
