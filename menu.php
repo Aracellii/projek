7f314ef8e7adc9fde3121b0f4710214ce46b2c71
Joestarya <aryarudysyahputra@gmail.com>
2024-11-06 11:43:56 +0700

    dsadasdasdsa

---
+++ menu.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!--header-->
+<body>
+    <!-- Header -->
+     <div class="header">
+        <div class="nav-container">
+            <div class="nav">
+                <div class="nav-brand">
+                    <h1><a href="">Food<span>Stall</span></a></h1>
+                </div>
+                <div class="nav-menu">
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                    <a href="">Daily Market</a>
+                    <a href="">Catalog</a>
+                </div>
+            </div>
+        </div>
+     </div>
+    <!--End Header-->
+
+<div class="food">
+    
+    <div class="container">
+        <div class="row">
+        <div class="col3">
+            <div class="food-card">
+                <div class="fc-image">
+                    <img src="https://images.unsplash.com/photo-1727403851978-46fd1fd82d9d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="img-responsive">
+                </div>
+                    <div class="fc-desc">
+                    <h3>INI AYAM MEWAH</h3>
+                    <p>CRISPY NYOi</p>
+                    </div>
+                <div class="fc-price">
+                    <div class="price-tag">Start from</div>
+                    <div class="price-num">Rp.20 <sup>Ribu</sup> </div>
+                </div>
+            </div>
+        </div>
+        <div class="col3">
+            <div class="food-card">
+                <div class="fc-image">
+                    <img src="https://images.unsplash.com/photo-1727403851978-46fd1fd82d9d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="img-responsive">
+                </div>
+                    <div class="fc-desc">
+                    <h3>INI AYAM MEWAH</h3>
+                    <p>CRISPY NYOi</p>
+                    </div>
+                <div class="fc-price">
+                    <div class="price-tag">Start from</div>
+                    <div class="price-num">Rp.20 <sup>Ribu</sup> </div>
+                </div>
+            </div>
+        </div><div class="col3">
+            <div class="food-card">
+                <div class="fc-image">
+                    <img src="https://images.unsplash.com/photo-1727403851978-46fd1fd82d9d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="img-responsive">
+                </div>
+                    <div class="fc-desc">
+                    <h3>INI AYAM MEWAH</h3>
+                    <p>CRISPY NYOi</p>
+                    </div>
+                <div class="fc-price">
+                    <div class="price-tag">Start from</div>
+                    <div class="price-num">Rp.20 <sup>Ribu</sup> </div>
+                </div>
+            </div>
+        </div><div class="col3">
+            <div class="food-card">
+                <div class="fc-image">
+                    <img src="https://images.unsplash.com/photo-1727403851978-46fd1fd82d9d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="img-responsive">
+                </div>
+                    <div class="fc-desc">
+                    <h3>INI AYAM MEWAH</h3>
+                    <p>CRISPY NYOi</p>
+                    </div>
+                <div class="fc-price">
+                    <div class="price-tag">Start from</div>
+                    <div class="price-num">Rp.20 <sup>Ribu</sup> </div>
+                </div>
+            </div>
+        
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
