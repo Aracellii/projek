491468bb79fc0c4a210bb92846ef6e63538f99d9
Joestarya <aryarudysyahputra@gmail.com>
2024-11-15 12:21:08 +0700

    baru lagi

--- pembayaran.php
+++ pembayaran.php
@@ -9,9 +9,28 @@
 <body>
     <div class="container">
         <div class="pembayaran">
-            <h1>PEMBAYARAN</h1>
+            <h1>PEMESANAN</h1>
             <div class="form-bayar">
-                <input type="text" placeholder="berapa pesanan">
+                <H1>PESANAN ATAS NAMA USERNAMES</H1>
+                <div>
+                <select class="form-bayar-input" name="cars" id="cars">
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">AYAM BAKAR PEDAS  </option>
+                    <option value="audi">Audi</option>
+                </select>
+                </div>
+                <input class="form-bayar-input" type="number" placeholder="jumlah">
+                <div>
+                <select class="form-bayar-input" name="cars" id="cars">
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">es teh tawar ga pake gula</option>
+                    <option value="audi">Audi</option>
+                </select>
+                </div>
+                <input class="form-bayar-input" type="number">
+                <button class="form-bayar-input" type="submit">PESAN SEKARANG</button>
             </div>
         </div>
     </div>
