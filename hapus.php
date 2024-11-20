7aed55630d926b20d8b5f917d2d94d74770882af
Aracelli <123230082@student.upnyk.ac.id>
2024-11-20 09:38:36 +0700

    hapus

--- hapus.php
+++ hapus.php
@@ -3,6 +3,8 @@
     include 'menu.php';
     include 'db.php';
     $qhapus = "DELETE FROM menu WHERE id_menu = $id";
+    $qhapusp = "DELETE FROM pembayaran WHERE id_menu = $id";
+    $hapusp=mysqli_query($conn,$qhapusp);
     $hasil = mysqli_query($conn, $qhapus);
     header("location: menu.php");
     ?>
