d715a36f5bdf7bd2b216965b1a33ac6ae7a7d2c3
Aracelli <123230082@student.upnyk.ac.id>
2024-11-30 10:27:16 +0700

    s

---
+++ cek_login.php
@@ -0,0 +1,22 @@
+<?php 
+session_start();
+$query = new mysqli('localhost', 'root', '', 'projek');
+// Mendapatkan username dan password dari input form
+$username = $_POST['username'];
+$password = $_POST['password'];
+// Menyeleksi data dari tabel 'user' dan 'admin' sekaligus
+$data = $query->query("SELECT 'user' AS role FROM user WHERE username='$username' AND password='$password'
+                       UNION
+                       SELECT 'admin' AS role FROM admin WHERE username='$username' AND password='$password'")
+          or die($query->error);
+
+// Mengecek apakah ada data yang ditemukan
+if ($data->num_rows > 0) {
+    $_SESSION['username'] = $username;
+    $_SESSION['status'] = "login";
+	$_SESSION['role'] = $row['role'];
+    header("location:session.php");
+} else {
+    header("location:login.php?pesan=gagal");
+}
+?>
---
+++ session.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+// Memeriksa apakah pengguna sudah login
+if (empty($_SESSION['username'])) {
+    header("location:login.php?pesan=belum_login");
+    exit();
+}
+?>
+<html>
+<head>
+  <title>Halaman Session</title>
+</head>
+<body>
+   <?php
+     // Menampilkan username
+     echo "Username: " . $_SESSION['username'] . "<br>";
+
+     // Menampilkan role  
+     echo "Role Anda: " . $_SESSION['username'] . "<br>";
+   ?>   
+<br>
+<a href="logout.php">Logout</a>
+</body>
+</html>
